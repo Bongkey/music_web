c07665b34e2e650585d59106e4b409b57bbe34fa
Ki.BongJo <Kibongjo1017@gmail.com>
2020-10-15 03:30:53 +0900

    #2 music play 가능
    1.main-screen
    앨범 커버
    앨범 타이틀
    레인지 바
    레인지 바 시간
    2.list-screen
    음악 재생 가능
    플레이어 타이틀 변경
    
    3. 공통
    플레이 아이콘 토글 기능

--- index.js
+++ index.js
@@ -1,26 +1,95 @@
 const heartBtn = document.querySelector('.heart-btn');
 const songs = document.querySelectorAll('.song');
 const heart = document.querySelector('.heart');
+const currentPlayTime = document.querySelector('.play__time p:first-child');
+const totalPlayTime = document.querySelector('.play__time p:last-child');
+const timeRange = document.querySelector('.play__range input[type=range]');
+const albumImg = document.querySelector('.main-screen .cover img');
+const playTitle = document.querySelector('.list-screen .play__text span');
+const mainTitle = document.querySelector('.main-screen .song__text h1');
+const playIcons = document.querySelectorAll('.fa-play');
+const songTitles = ['EIGHT', 'Blueming', 'BBIBBI', 'Palette'];
+const fileTitles = ['eight', 'blueming', 'bbibbi', 'palette'];
+let preMusic = "";
 
-heart.style.animation = "heartBeat 2s linear infinite";
+const HEART_ANIMATION = "heartBeat 2s linear infinite";
+heart.style.animation = HEART_ANIMATION;
+
 heartBtn.addEventListener("click", heartBtnHandler);
-
 songs.forEach(song => {
     song.addEventListener("click", songHandler);
 });
+timeRange.addEventListener("input", rangeHandler);
 
 function heartBtnHandler() {
-    heart.style.animation = heart.style.animation ? "" : "heartBeat 2s linear infinite";
+    heart.style.animation = heart.style.animation ? "" : HEART_ANIMATION;
     heartBtn.style.fontWeight = heartBtn.style.fontWeight ? "" : 800;
     heartBtn.style.color = heartBtn.style.color === "tomato" ? "#000" : "tomato";
 }
 
 function songHandler(event) {
+    const songTitle = event.currentTarget.querySelector('span').innerText;
+    const songIndex = searchSongIndex(songTitle);
+    const music = new Audio('audio/' + fileTitles[songIndex]+'.mp3');
+    albumImg.src = ('img/'+ fileTitles[songIndex]+'.jpg');
+    playTitle.innerText = songTitles[songIndex];
+    mainTitle.innerText = songTitles[songIndex];
+
     const otherSong = document.querySelector('.selected-song');
+
+    if (preMusic.src === music.src) {
+        preMusic.pause();
+        music.pause();
+    } else if (preMusic === "") {
+        music.play();
+    } else if (preMusic !== music) {
+        preMusic.pause();
+        music.play();
+    }
+
+    preMusic = music;
+    // 다른 .song 클릭시
     if (otherSong && otherSong !== event.currentTarget) {
         otherSong.classList.toggle('selected-song');
     }
     // //currentTarget -> 상위 요소
     event.currentTarget.classList.toggle('selected-song');
+}
 
+function searchSongIndex(songTitle) {
+    for (i = 0; i < songTitles.length; i++) {
+        if (songTitle === songTitles[i]) {
+            return (songTitles.indexOf(songTitles[i]));
+        }
+    }
+}
+
+setInterval(function () {
+    if (preMusic !== "") {
+        const currentTime = parseInt(preMusic.currentTime);
+        const duration = parseInt(preMusic.duration);
+        timeRange.max = duration;
+        timeRange.value = currentTime;
+        const currentTimes = {
+            min: (parseInt(currentTime / 60)),
+            sec: (currentTime % 60)
+        }
+        const durations = {
+            min: (parseInt(duration / 60)),
+            sec: (duration % 60)
+        }
+        currentPlayTime.innerText = `${currentTimes.min}:${currentTimes.sec}`;
+        totalPlayTime.innerText = `${durations.min}:${durations.sec}`;
+        playIconsToggle(preMusic.paused);
+    }
+}, 500);
+
+function rangeHandler (event) {
+    preMusic.currentTime = event.target.value;
+}
+
+function playIconsToggle (musicState) {
+    playIcons.forEach(icon => {
+        icon.className = musicState ? "fas fa-play" : "fas fa-pause";
+    })
 }
